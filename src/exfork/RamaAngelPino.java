fc583f87637f7cd941ecc70a4592dba32d0bb9fb
apinodominguez <apinodominguez@nautilus3.danielcastelao.org>
2018-12-13 08:48:53 +0100

    ExFork. Rama AngelPino creada.

---
+++ src/exfork/RamaAngelPino.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exfork;
+
+/**
+ *
+ * @author apinodominguez
+ */
+public class RamaAngelPino {
+    public void operacion(){
+        int x = 1;
+        int y = 2;
+        int suma = x + y;
+    }
+}
