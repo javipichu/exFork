295172e843a3e564439a42eebce95ffde18e388e
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2018-12-13 08:55:16 +0100

    Cree clase java con mi nombre para el examen

---
+++ src/exfork/EmilioMendez.java
@@ -0,0 +1,16 @@
+package exfork;
+
+/**
+ * Clase base para el examen de 1ºDAM
+ * @author Damian Nogueiras
+ */
+public class ExFork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Clase creada para el examen
+    }
+    
+}
